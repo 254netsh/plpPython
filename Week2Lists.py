460ca0314311fb01eca7f5a5e3fd28b35bcaecb9
sam <samgachuiga@gmail.com>
2024-03-20 07:52:30 +0300

    Signed-off-by: sam <samgachuiga@gmail.com>

---
+++ Week2Lists.py
@@ -0,0 +1,20 @@
+#an empty list
+myList =[]
+
+#append 10,20,30,40
+myList.append(10)
+myList.append(20)
+myList.append(30)
+myList.append(40)
+
+#insert 15 at second position
+myList.insert(1,15)
+
+#remove last element
+myList.pop()
+
+myList.sort()
+
+#find and print index of 30
+Index30=myList.index(30)
+print("new list", myList)
